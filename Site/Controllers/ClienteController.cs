3a2e31af224bc93128510b08a553bda1d10fa0f8
Samuel Guedes de Vasconcelos <samuel.vasconcelos@live.com>
2016-01-06 09:45:44 -0200

    Inicial

---
+++ Site/Controllers/ClienteController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        BeFitnessBREntities db = new BeFitnessBREntities();
+
+        //
+        // GET: /Cliente/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cliente.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+
+
+
+	}
+}
